2dcd32c3bef1335b35417fa4f274901846a7512f
Hyurim <kyo2301@naver.com>
2025-10-27 02:36:07 +0900

    chore(config): 설정 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: "window",
+    "process.env": {},
+  },
   resolve: {
     dedupe: ["react", "react-dom"],
   },
@@ -10,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8081",
         changeOrigin: true,
       },
     },
